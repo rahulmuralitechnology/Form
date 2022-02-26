f541d6702953b6625fe23e6d1193f37cd2eb1700
rahulmuralitechnology <rahulmuralitechnology@gmail.com>
2022-02-25 22:01:20 -0800

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
- function validation()
+function fnvalidation()
+ {
+     var form = document.getElementById("form");
+     var first = document.getElementById("first").value;
+     var fname = document.getElementById("fname");
+     var pattern = /^([A-Za-z. ']+)$/;
+
+     if (first.match(pattern))
+     {
+         form.classList.add("valid");
+         form.classList.remove("invalid");
+         fname.innerHTML = "";
+         fname.style.color = "#00ff00";
+     }
+     else
+     {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        fname.innerHTML = "Please enter Name";
+        fname.style.color = "#ff0000";
+     }
+    }
+    function lnvalidation()
+ {
+     var form = document.getElementById("form");
+     var last = document.getElementById("last").value;
+     var lname = document.getElementById("lname");
+     var pattern = /^([A-ZÀ-ÿ][-,a-z. ']+)$/;
+
+     if (last.match(pattern))
+     {
+         form.classList.add("valid");
+         form.classList.remove("invalid");
+         lname.innerHTML = "";
+         lname.style.color = "#00ff00";
+     }
+     else
+     {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        lname.innerHTML = "Please enter Name";
+        lname.style.color = "#ff0000";
+     }
+    }
+  function evalidation()
  {
      var form = document.getElementById("form");
      var email = document.getElementById("email").value;
@@ -27,14 +71,36 @@
          text.style.color = "#00ff00";
         }
     }
+    function pnvalidation()
+ {
+     var form = document.getElementById("form");
+     var phone = document.getElementById("phone").value;
+     var pno = document.getElementById("pno");
+     var pattern = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+
+     if (phone.match(pattern))
+     {
+         form.classList.add("valid");
+         form.classList.remove("invalid");
+         pno.innerHTML = "";
+         pno.style.color = "#00ff00";
+     }
+     else
+     {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        pno.innerHTML = "Please enter valid Phone number";
+        pno.style.color = "#ff0000";
+     }
+    }
     function pvalidation()
  {
     var form = document.getElementById("form");
      var Password = document.getElementById("Password").value;
      var psd = document.getElementById("psd");
+     var mpp =/^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))$/;
      var spp =/^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))$/;
-     var mpp =/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})$/;
- 
+     
      if (Password.match(spp))
      {
          form.classList.add("valid");
@@ -47,7 +113,7 @@
         form.classList.add("valid");
         form.classList.remove("invalid");
         psd.innerHTML = "Medium";
-        psd.style.color = "#ff0000";
+        psd.style.color = "#ffea00";
      }
      else     
      {
