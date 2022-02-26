07730fff978bd5d09d477ec912e252f4f25f5d14
rahulmuralitechnology <rahulmuralitechnology@gmail.com>
2022-02-25 18:20:12 -0800

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
      var text = document.getElementById("text");
      var pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
 
-     
      if (email.match(pattern))
      {
          form.classList.add("valid");
@@ -26,5 +25,35 @@
          form.classList.remove("invalid");
          text.innerHTML = "";
          text.style.color = "#00ff00";
+        }
+    }
+    function pvalidation()
+ {
+    var form = document.getElementById("form");
+     var Password = document.getElementById("Password").value;
+     var psd = document.getElementById("psd");
+     var spp =/^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))$/;
+     var mpp =/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})$/;
+ 
+     if (Password.match(spp))
+     {
+         form.classList.add("valid");
+         form.classList.remove("invalid");
+         psd.innerHTML = "Strong";
+         psd.style.color = "#00ff00";
      }
- }
+     else if(Password.match(mpp))
+     {
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        psd.innerHTML = "Medium";
+        psd.style.color = "#ff0000";
+     }
+     else     
+     {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        psd.innerHTML = "Weak";
+         psd.style.color = "#ff0000";
+     }
+    }
